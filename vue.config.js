81ed59007b29501338be0ebdae7713978a79f57a
quannh <quannh@runsystem.net>
2025-03-28 09:39:15 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  publicPath: process.env.VUE_APP_OFFLINE_MODE ? './' : '/',
+  publicPath: process.env.VUE_APP_OFFLINE_MODE ? './' : process.env.VUE_APP_PUBLICH_PATH,
   transpileDependencies: true,
   configureWebpack: {
+    devServer: {
+      historyApiFallback: true
+    },
     plugins: [
       new webpack.DefinePlugin({
         // Vue CLI is in maintenance mode, and probably won't merge my PR to fix this in their tooling
@@ -12,5 +15,12 @@ module.exports = defineConfig({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
       })
     ],
+  },
+
+  devServer: {
+    // open: process.platform === 'darwin',
+    // host: '0.0.0.0',
+    port: 8081,
+    https: false,
   },
 })
