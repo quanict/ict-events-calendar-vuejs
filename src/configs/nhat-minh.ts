b51edebae7a3f720052eadf569a88e9a846791ec
quanict <hongquan2712@gmail.com>
2025-04-16 19:46:10 +0700

    update

--- src/configs/nhat-minh.ts
+++ src/configs/nhat-minh.ts
@@ -1,7 +1,10 @@
 // export const domain = 'https://github.com/khieu-ngoan'
 export const domain = "http://calendar.loc/nhat-minh"
 export const paths = {
+    "nhat-minh-23-p2" : ["2023-06-01", "2023-12-29"],
+    "nhat-minh-24" : ["2024-01-01", "2024-04-28"],
+    "nhat-minh-24-p2" : ["2024-05-01", "2024-07-31"],
     "nhat-minh-24-p3" : ["2024-08-01", "2024-12-31"],
-    //"nhat-minh-25-p1"
+    "nhat-minh-25-p1" : ["2025-01-01", "2025-06-30"],
 }
 
