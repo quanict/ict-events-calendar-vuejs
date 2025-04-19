507d74de4297e709886b60a791a43ada05af2daa
quannh <quannh@runsystem.net>
2025-04-19 18:04:49 +0700

    update

--- src/views/YearView.vue
+++ src/views/YearView.vue
@@ -1,10 +1,23 @@
 <template>
   <DavidUiLayout>
-    <div class="text-3xl font-bold mt-10 ">{{ year }}</div>
+
+    <div class="monthy-header">
+      <div class="btn-previous">
+        <button v-on:click="previous_year"><i class="fa-solid fa-angles-left"></i></button>
+      </div>
+
+      <div class="text-3xl font-bold mt-10 ">{{ year }}</div>
+
+      <div class="btn-next">
+        <button v-on:click="next_year"><i class="fa-solid fa-angles-right"></i></button>
+      </div>
+    </div>
 
     <div class="year">
       <template v-for="i in 12">
-        <Month :month="`${year}-${i}`"/>
+        <div :id="`month-${year}-${i}`">
+          <Month :month="`${year}-${i}`" :ref="`month-${year}-${i}`" />
+        </div>
       </template>
     </div>
   </DavidUiLayout>
@@ -14,12 +27,15 @@
 https://vi.vuejs.org/v2/style-guide/#Dat-ten-component-dung-nhieu-tu-thiet-yeu
 */
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent, ref, useTemplateRef } from 'vue';
 import DavidUiLayout from '@/layouts/DavidUiLayout.vue';
 import Month from "@/components/month/Month.vue"
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 
-@Options({
+/**
+ * https://vueuse.org/core/useScroll/
+ */
+export default defineComponent({
   components: {
     DavidUiLayout,
     Month
@@ -30,15 +46,47 @@ import moment from 'moment';
   },
 
   data() {
-    return{
-      year : 2024
+    return {
+      year: ''
     }
   },
 
-  created(){
+  beforeCreate() {
+
+
+  },
+  mounted() {
+
+  },
+  created() {
     this.year = moment().format('YYYY')
     document.title = `year ${this.year}`
+    this.scrollToCurrent()
+
   },
+
+  methods: {
+    previous_year(): void {
+      this.year = moment(this.year, 'YYYY').add(-1, "year").format('YYYY')
+      document.title = `year ${this.year}`
+    },
+    next_year(): void {
+      this.year = moment(this.year, 'YYYY').add(1, "year").format('YYYY')
+      document.title = `year ${this.year}`
+    },
+    scrollToCurrent() {
+      setTimeout(function () {
+        const monthRef = `month-${moment().format("YYYY-M")}`
+        const currentMonth = document.getElementById(monthRef)
+        console.log(`=== find month`, { currentMonth, monthRef })
+        if (currentMonth) {
+          currentMonth.scrollIntoView({ behavior: "smooth"})
+        }
+      }, 500);
+
+    },
+  },
+
 })
-export default class YearView extends Vue { }
+
 </script>
